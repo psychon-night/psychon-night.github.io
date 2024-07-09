910f7d8cd7e1e530908fbbc3da938fda063a119e
Psychon <49412250+psychon-night@users.noreply.github.com>
2024-07-08 19:39:17 -0700

    Trust me, nothing changed

---
+++ resources/js/gameEgg.js
@@ -0,0 +1,12 @@
+function rambleyEgg() {
+	window.scrollTo({ top: 0, behavior: 'smooth' });
+	
+	document.getElementById('profileLogoLink').href='https://tenor.com/view/indigo-park-gif-27580131544399364'
+	document.getElementById('profileLogo').src='https://media.tenor.com/9ou0DVD5SbwAAAAM/rambley-the-raccoon-indigo-park.gif'
+
+	document.getElementById('text02').innerHTML='Average Rambley Addict'
+
+	document.getElementById('rambleySwitch').innerHTML='Rambley the Raccoon'
+	document.getElementById('rambleySwitch').onclick=''
+	document.getElementById('rambleySwitch').style='cursor:text'
+}
